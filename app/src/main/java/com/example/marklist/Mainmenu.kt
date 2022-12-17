a54d57b5be24a5c8e58cfc41fa7c48c005ee316b
Rohith Jikki <rohith11jr@gmail.com>
2022-12-17 21:55:21 +0530

    Spinner

--- app/src/main/java/com/example/marklist/Mainmenu.kt
+++ app/src/main/java/com/example/marklist/Mainmenu.kt
@@ -2,10 +2,17 @@ package com.example.marklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Mainmenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainmenu)
+        val classList = listOf("VI","VII","VIII","IX","X",)
+        val list = findViewById<Spinner>(R.id.class_spinner)
+        val arrayAdapter = ArrayAdapter(this,R.layout.spinner_list,classList)
+        list.adapter = arrayAdapter
+
     }
 }
