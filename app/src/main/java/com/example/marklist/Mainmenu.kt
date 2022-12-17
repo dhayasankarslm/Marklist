6098c26773acee4fd97a125c1f8c64dedd29c6cc
dhayasankarslm <dhayasankarslm@gmail.com>
2022-12-17 21:16:00 +0530

    correct xml

---
+++ app/src/main/java/com/example/marklist/Mainmenu.kt
@@ -0,0 +1,11 @@
+package com.example.marklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mainmenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainmenu)
+    }
+}
