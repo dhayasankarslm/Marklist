8a58817dbfb0c7cf27d0c1ae82fd949de65ac299
Rohith Jikki <rohith11jr@gmail.com>
2022-12-17 13:25:59 +0530

    initial commit

--- app/src/main/java/com/example/marklist/MainActivity.kt
+++ app/src/main/java/com/example/marklist/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.marklist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val signUp = findViewById<Button>(R.id.button2)
+        signUp.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+        }
     }
 }
