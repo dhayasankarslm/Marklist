bbb01ecbd493031908d4bedf77b7fa1de6bf100a
Rohith Jikki <rohith11jr@gmail.com>
2022-12-20 20:00:58 +0530

    register fix

--- app/src/main/java/com/example/marklist/StudentList.kt
+++ app/src/main/java/com/example/marklist/StudentList.kt
@@ -13,17 +13,31 @@ class StudentList : AppCompatActivity() {
         setContentView(R.layout.activity_student_list)
         val registerButton = findViewById<Button>(R.id.registerButton)
         val studentName = findViewById<EditText>(R.id.studentName)
+        val studentNumber = findViewById<EditText>(R.id.studentNumber)
         val grade = intent.getStringExtra("grade").toString()
         val section = intent.getStringExtra("section").toString()
 
         val mDatabase = FirebaseDatabase.getInstance("https://marklist-e101f-default-rtdb.asia-southeast1.firebasedatabase.app/").reference
         registerButton.setOnClickListener {
-            val studentInstance = Student(studentName.text.toString())
-            mDatabase.child(grade).child(section).child("1").setValue(studentInstance).addOnCompleteListener {
-                if (it.isComplete) {
-                    Toast.makeText(this, "successfully added", Toast.LENGTH_LONG).show()
+            val studentNameValue = studentName.text.toString()
+            val studentNumberValue = studentNumber.text.toString()
+            if(studentNameValue != ""){
+                Toast.makeText(this,"Enter a name",Toast.LENGTH_SHORT).show()
+                studentName.requestFocus()
+            }
+            else if (studentNumberValue != ""){
+                Toast.makeText(this,"Enter Roll Number",Toast.LENGTH_SHORT).show()
+                studentNumber.requestFocus()
+            }
+            else{
+                val studentInstance = Student(studentName.text.toString())
+                mDatabase.child(grade).child(section).child(studentNumberValue).setValue(studentInstance).addOnCompleteListener {
+                    if (it.isComplete) {
+                        Toast.makeText(this, "successfully added", Toast.LENGTH_LONG).show()
+                    }
                 }
             }
+
         }
     }
 }
